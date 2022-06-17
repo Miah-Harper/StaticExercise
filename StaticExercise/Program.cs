058c86bccc4ebfffcaf2635dd43c233b0ff88d31
Miah Harper <harpermiah@yahoo.com>
2022-06-16 20:55:00 -0400

    finsihed exercise

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fahrenheit = TempConverter.FahrenheitToCelcius(78);
+            var celsius = TempConverter.CelsiusToFahrenheit(67);
+
+            Console.WriteLine($"It's {fahrenheit} degrees fahrenheit");
+            Console.WriteLine($"It's {celsius} degrees celsius");
+        }
+    }
+}
+
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace StaticExercise
+{
+	public static class TempConverter
+	{
+
+		public static double FahrenheitToCelcius(double fahrenheit)
+		{
+			var result = (fahrenheit);
+
+			return result;
+		}
+
+		public static double CelsiusToFahrenheit(double celsius)
+		{
+			var result = (celsius);
+			return result;
+		}
+	}
+
+    
+
+
+}
